4cee9e9a786fe3d273833ce0897e649e46b0371a
Ricardo Velázquez Contreras <rivel_co@protonmail.com>
2020-10-28 22:17:09 -0600

    Updated version
    Now the greetings function shows the version v1 instead the v1-alpha

--- RDO_files/greetings.py
+++ RDO_files/greetings.py
@@ -1,4 +1,4 @@
 def greetings():
-    print("\n>>           Rack Database Organizer v1-alpha           <<")
+    print("\n>>           Rack Database Organizer v1           <<")
     print("Please visit https://github.com/rivelco/Rack-database-organizer")
     print("for documentation and latest version.\n")
